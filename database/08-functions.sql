9fa00cd880600ecac22c90009e841448a1308489
Jose Javier Sanahuja <82359964+josejaviersanahuja@users.noreply.github.com>
2023-05-30 20:29:19 +0100

    Update 08-functions.sql get_agenda_info

--- database/08-functions.sql
+++ database/08-functions.sql
@@ -429,3 +429,43 @@ EXCEPTION
         RETURN -1;
 END;
 $$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION get_agenda_info(_fecha DATE)
+RETURNS JSON AS $$
+DECLARE
+    agenda_info JSON;
+BEGIN
+    -- Obtener la información de la agenda y el tema del restaurante para la fecha especificada
+    SELECT
+        json_build_object(
+            'fecha', a.fecha,
+            'theme_name', rt.theme_name,
+            'image_url', rt.image_url,
+            'capacidad a las 19:00', a.t1900,
+            'capacidad a las 19:15', a.t1915,
+            'capacidad a las 19:30', a.t1930,
+            'capacidad a las 19:45', a.t1945,
+            'capacidad a las 20:00', a.t2000,
+            'capacidad a las 20:15', a.t2015,
+            'capacidad a las 20:30', a.t2030,
+            'capacidad a las 20:45', a.t2045,
+            'capacidad a las 21:00', a.t2100,
+            'capacidad a las 21:15', a.t2115,
+            'capacidad a las 21:30', a.t2130,
+            'capacidad a las 21:45', a.t2145
+        ) INTO agenda_info
+    FROM
+        agenda AS a
+    JOIN
+        restaurant_themes AS rt ON a.restaurant_theme_id = rt.id
+    WHERE
+        a.fecha = _fecha;
+    
+    RETURN agenda_info;
+    
+EXCEPTION
+    WHEN OTHERS THEN
+        -- Capturar y devolver el error como objeto JSON
+        RETURN json_build_object('error', SQLERRM);
+END;
+$$ LANGUAGE plpgsql;
