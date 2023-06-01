a1e7af52b89c0ea21cfb1a29fb53d39e0e82dfd3
José Javier Sanahuja <josejaviersanahuja@gmail.com>
2023-06-01 11:47:47 +0100

    introduciendo deno para el backend

---
+++ server/first_steps.ts
@@ -0,0 +1,7 @@
+const filenames = Deno.args;
+console.log(filenames);
+
+for (const filename of filenames) {
+  const file = await Deno.open(filename);
+  await file.readable.pipeTo(Deno.stdout.writable, { preventClose: true });
+}
