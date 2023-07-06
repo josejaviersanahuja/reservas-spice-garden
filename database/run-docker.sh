7c7ec0eb3bdcf24cc5a339ddd0acc186ce3c05f3
José Javier Sanahuja <josejaviersanahuja@gmail.com>
2023-07-06 12:22:53 +0000

    ignoring pgdata from dockerimg fix rundocker

--- database/run-docker.sh
+++ database/run-docker.sh
@@ -3,4 +3,5 @@ docker run -d \
 	-e POSTGRES_PASSWORD=password \
 	-e PGDATA=/var/lib/postgresql/data/pgdata \
 	-v /home/ec2-user/reservas-spice-garden/database:/var/lib/postgresql/data \
+	-p 5432:5432 \
 	postgres
